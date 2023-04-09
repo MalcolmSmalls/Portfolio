3ef0fc68c3f8774475416dc8fc9b0d2900931c45
MalcolmSmalls <smallsbeats@gmail.com>
2023-04-09 11:22:57 -0700

    Add responsiveness to skillset

--- frontend/src/Components/Skillset.jsx
+++ frontend/src/Components/Skillset.jsx
@@ -7,15 +7,15 @@ export default function Skillset() {
       <h2 className='text-white font-BebasNeue text-5xl text-center underline underline-offset-8 decoration-red-500 decoration-double py-20'>
         Skillset
       </h2>
-      <div className='container font-Raleway flex p-5 mx-auto w-2/3 gap-10'>
+      <div className='container font-Raleway flex p-5 mx-auto lg:w-2/3 w-full gap-10 lg:flex-row flex-col'>
         <div className='container flex flex-col'>
-          <div className='container flex gap-5'>
-            <div className='bg-[#1e1e1e] rounded h-14 w-14 flex justify-center items-center shadow-lg flex-none'>
+          <div className='container flex  gap-5'>
+            <div className='bg-[#1e1e1e] rounded h-14 w-14 flex justify-center items-center shadow-lg flex-none '>
               <i className='fab fa-html5 text-red-500 text-3xl'></i>
             </div>
             <div className='container flex flex-col'>
               <h3 className='text-white font-bold text-2xl pb-2'>HTML/CSS</h3>
-              <p className='text-[#747474] font-semibold pb-14'>
+              <p className='text-[#747474] font-semibold lg:pb-14 pb-8'>
                 Learning from The Odin Project and #100Devs, I'm able to build
                 websites from scratch using semantically correct HTML tags with
                 mobile-first, responsive CSS.
@@ -29,7 +29,7 @@ export default function Skillset() {
             </div>
             <div className='container flex flex-col'>
               <h3 className='text-white font-bold text-2xl pb-2'>JavaScript</h3>
-              <p className='text-[#747474] font-semibold pb-14'>
+              <p className='text-[#747474] font-semibold lg:pb-14 pb-8'>
                 After completing multiple JavaScript projects which included
                 building 'Battleship' and 'Tic-Tac-Toe', I've learned a lot of
                 vanilla JS for DOM manipulation in order to provide an
@@ -44,7 +44,7 @@ export default function Skillset() {
             </div>
             <div className='container flex flex-col'>
               <h3 className='text-white font-bold text-2xl pb-2'>React.JS</h3>
-              <p className='text-[#747474] font-semibold pb-14'>
+              <p className='text-[#747474] font-semibold lg:pb-14'>
                 Adding even more complex interactivity, React.JS complements my
                 vanilla JS knowledge with the power of JSX. I am able to build
                 scalable games and web apps that interact with APIs.
@@ -77,7 +77,7 @@ export default function Skillset() {
             </div>
             <div className='container flex flex-col'>
               <h3 className='text-white font-bold text-2xl pb-2'>Node.JS</h3>
-              <p className='text-[#747474] font-semibold pb-14'>
+              <p className='text-[#747474] font-semibold lg:pb-14 pb-8'>
                 Leveraging both Node.JS and Express.JS, I am able to build
                 RESTful APIs in order to create full stack web applications.
                 Authentication, encryption, database creation and deployment are
