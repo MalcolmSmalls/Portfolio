360ab15dd3743a0cd6c9e012d78dbfce808fc194
MalcolmSmalls <smallsbeats@gmail.com>
2023-04-13 12:45:08 -0700

    Fix header for mobile

--- frontend/src/Components/Header.jsx
+++ frontend/src/Components/Header.jsx
@@ -7,15 +7,15 @@ export default function Header() {
   return (
     <>
       <div
-        className='lg:h-screen h-[90vh] w-full lg:bg-fill bg-fill lg:bg-top bg-[14%_5%]'
+        className='lg:h-screen h-[90vh] w-full lg:bg-fill bg-fill lg:bg-top bg-[20%_20%]'
         style={{ backgroundImage: `url(${bg1})` }}
         id='home'
       >
-        <div className='flex bg-[#1e1e1e] lg:h-screen h-full w-full bg-opacity-80 justify-center items-center flex-col lg:gap-10 gap-6'>
-          <h1 className='text-white lg:text-9xl text-7xl font-BebasNeue'>
+        <div className='flex bg-[#1e1e1e] lg:h-screen h-full w-full lg:bg-opacity-80 bg-opacity-90 justify-center  lg:pt-0 pt-14 items-center flex-col lg:gap-10 gap-2'>
+          <h1 className='text-white lg:text-9xl text-6xl font-BebasNeue'>
             Malcolm Smalls
           </h1>
-          <h3 className='text-white font-BebasNeue lg:text-5xl text-3xl'>
+          <h3 className='text-white font-BebasNeue lg:text-5xl text-xl'>
             <Typewriter
               options={{
                 strings: [
@@ -31,7 +31,7 @@ export default function Header() {
               }}
             />
           </h3>
-          <button className='text-red-500 text-xl font-Barlow border border-red-500 py-2 px-6 rounded-2xl hover:bg-red-500 hover:text-white tracking-wide transition-colors duration-500'>
+          <button className='text-red-500 lg:text-xl text-base font-Barlow border border-red-500 py-2 px-6 rounded-2xl hover:bg-red-500 hover:text-white tracking-wide transition-colors duration-500'>
             <a href='#contact'>Let's Work</a>
           </button>
         </div>
